46c137269776a954d57130ec0eeebc9ce34af668
MUSHARIB-97 <mushariburrab9@gmail.com>
2024-10-28 12:31:01 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import AppRoute from "./routes/Routing";
 import { Provider } from "react-redux";
 import { store } from "./redux-store/store.js";
+import AppRoute from "./routes/Routing.js";
 function App() {
   return (
     <Provider store={store}>
